97c33c6951bb1526e40ebaa6b2b0b61f4394a32e
Christian Abarca <cabarca@wearegap.com>
2023-08-21 15:58:40 -0600

    Use secret pass

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -22,7 +22,7 @@ describe('example to-do app', () => {
 
   it('can add new todo items', () => {
     cy.get('#username').type('student')
-    cy.get('#password').type('${{ secrets.SECRET_TOKEN }}')
+    cy.get('#password').type('`${{ secrets.TEST_HELLO }}`')
     cy.get('#submit').click()
   })
 })
